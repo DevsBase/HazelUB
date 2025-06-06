8d4760e0aa23f59f7a9bb28d6761709ae9045c6e
Otazuki004 <otazuki004@gmail.com>
2025-06-06 13:03:02 +0000

    idk

--- Essentials/CreateClients.py
+++ Essentials/CreateClients.py
@@ -7,10 +7,10 @@ log = logging.getLogger(__name__)
 
 class CreateClients:
   def __init__(self, other):
-    config = other.output
-    API_ID, API_HASH = config.get('API_ID'), config.get('API_HASH')
-    PYROGRAM_SESSION, BOT_TOKEN = config.get("PYROGRAM_SESSION"), config.get("BOT_TOKEN")
-    MONGO_DB_URL = config.get('MONGO_DB_URL')
+    self.config = other.output
+    API_ID, API_HASH = self.config.get('API_ID'), self.config.get('API_HASH')
+    PYROGRAM_SESSION, BOT_TOKEN = self.config.get("PYROGRAM_SESSION"), self.config.get("BOT_TOKEN")
+    MONGO_DB_URL = self.config.get('MONGO_DB_URL')
     self.app = Client("UB", session_string=PYROGRAM_SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Hazel/plugins"))
     if len(BOT_TOKEN) > 50: raise Exception("Bot token length too high please check your token.")
     self.bot = Client("Bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
@@ -18,5 +18,5 @@ class CreateClients:
     self.DATABASE = AsyncIOMotorClient(MONGO_DB_URL)["Something"]
     from MultiSessionManagement import add_client
     add_client(self.app)
-    for i in config.get('OtherSessions'):
+    for i in self.config.get('OtherSessions'):
       add_client(Client("otherClient", session_string=i, api_id=API_ID, api_hash=API_HASH))
