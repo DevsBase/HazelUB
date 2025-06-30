6ff7ec0eac18fac722cc8f1d2840dae70e1fcb42
Otazuki004 <otazuki004@gmail.com>
2025-06-30 18:46:42 +0000

    .

--- Essentials/CreateClients.py
+++ Essentials/CreateClients.py
@@ -10,7 +10,8 @@ class CreateClients:
     API_ID, API_HASH = config.get('API_ID'), config.get('API_HASH')
     PYROGRAM_SESSION, BOT_TOKEN = config.get("PYROGRAM_SESSION"), config.get("BOT_TOKEN")
     MONGO_DB_URL = config.get('MONGO_DB_URL')
-    self.app = Client("UB", session_string=PYROGRAM_SESSION, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="Hazel/plugins"))
+    from .vars import __version__
+    self.app = Client("UB", session_string=PYROGRAM_SESSION, api_id=API_ID, api_hash=API_HASH, device_model="HazelUB", system_version=f"HazelUB (v{__version__})", plugins=dict(root="Hazel/plugins"))
     if len(BOT_TOKEN) > 50: raise Exception("Bot token length too high please check your token.")
     self.bot = Client("Bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
     self.nexbot = Nexgram.Client("NexgramBot",BOT_TOKEN)
@@ -18,4 +19,4 @@ class CreateClients:
     from MultiSessionManagement import add_client
     add_client(self.app)
     for i in config.get('OtherSessions'):
-      add_client(Client("otherClient", session_string=i, api_id=API_ID, api_hash=API_HASH))
+      add_client(Client("otherClient", session_string=i, api_id=API_ID, api_hash=API_HASH, device_model="HazelUB", system_version=f"HazelUB (v{__version__})"))
--- session_gen.py
+++ session_gen.py
@@ -1,5 +1,7 @@
 from pyrogram import Client
 import asyncio
+from Essentials.vars import __version__
+import os
 
 while (True):
   try: api_id = int(input('Enter your API_ID. (get from my.telegram.org): '))
@@ -8,9 +10,15 @@ while (True):
   if len(api_hash) < 13: continue
   break
 
-client = Client("client", api_id=api_id, api_hash=api_hash)
+name = 'client'
+client = Client(name, api_id=api_id, api_hash=api_hash, device_model="HazelUB", system_version=f"HazelUB (v{__version__})")
 
 async def main():
   await client.start()
+  session = await client.export_session_string()
+  print("SESSION STRING GENERATED\n")
+  print(session)
+  try: os.remove(f'{name}.session')
+  except: print(f"Delete {name}.session (if created). To be secure and don't share session to anyone.")
   
 asyncio.run(main())
