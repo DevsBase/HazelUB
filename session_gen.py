a989fa6fbe2a1f1e025ab944e9bf8a8a4477e958
Otazuki004 <otazuki004@gmail.com>
2025-06-30 18:29:14 +0000

    .

--- session_gen.py
+++ session_gen.py
@@ -1,7 +1,14 @@
 from pyrogram import Client
 import asyncio
 
-client = Client("client")
+while (True):
+  try: api_id = int(input('Enter your API_ID. (get from my.telegram.org): '))
+  except: continue
+  api_hash = input('Enter your API_HASH. (get from my.telegram.org): ')
+  if len(api_hash) < 13: continue
+  break
+
+client = Client("client", api_id=api_id, api_hash=api_hash)
 
 async def main():
   await client.start()
