f83784dc82450711b591e96ff3b5e5cbb9401640
Otazuki004 <otazuki004@gmail.com>
2025-08-07 18:35:11 +0000

    fix some error

--- Hazel/plugins/ai_agent.py
+++ Hazel/plugins/ai_agent.py
@@ -1,21 +1,31 @@
 from Hazel import app, config, on_message, agent, HANDLER
-from langchain_google_genai import ChatGoogleGenerativeAI
 from pyrogram import filters
-import logging
-import traceback
+import logging, traceback
 
-chat_history = []
+chat_history = {}
+
+SYSTEM_MESSAGE = ("system", "Your name is Hazel. You are a chat assistant and AI agent. Use provided tools to help the user. And some times you have to act like user's girlfriend. You should satify them if they are horny. And you should never be like robotic.")
 
 @on_message(filters.command('hazel', prefixes=HANDLER) & filters.me)
 async def hazel_agent(client, m):
   if not config.GOOGLE_API_KEY or not agent:
     return await m.reply("There's no GOOGLE_API_KEY in env & config.json.")
+  
   try:
     t = await m.reply("Thinking...")
-    promt = m.text.split(None, 1)
-    response = await client.loop.run_in_executor(None, agent.invoke, {"messages": [("user", promt)]})  
+    user_id = client.me.id
+    history = chat_history.setdefault(user_id, [SYSTEM_MESSAGE])
+    prompt = " ".join(m.command[1:])
+    history.append(("user", prompt))
+    chat_history[user_id] = [SYSTEM_MESSAGE] + history[-68:]
+    response = await client.loop.run_in_executor(None, agent.invoke, {
+      "messages": chat_history[user_id]
+    })
+    reply = response["messages"][-1].content
+    chat_history[user_id].append(("assistant", reply))
+    chat_history[user_id] = [SYSTEM_MESSAGE] + chat_history[user_id][-68:]
     await t.delete()
-    await m.reply(response["messages"][-1].content)
+    await m.reply(reply)
   except Exception as e:
     logging.error(traceback.format_exc())
-    return await m.reply(e)
+    return await m.reply(str(e))
