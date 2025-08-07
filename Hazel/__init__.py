c9b8b8d6fe924c116df085c9c1e2cd2a958fdaf1
Otazuki004 <otazuki004@gmail.com>
2025-08-07 17:23:37 +0000

    fix AttributeError

--- Hazel/__init__.py
+++ Hazel/__init__.py
@@ -4,6 +4,5 @@ from MultiSessionManagement.decorators import *
 config = Init()
 clients = CreateClients(config)
 app, bot = clients.app, clients.bot
-nexbot, DATABASE = clients.nexbot, clients.DATABASE
 
 llm, agent = create_agent(config)
