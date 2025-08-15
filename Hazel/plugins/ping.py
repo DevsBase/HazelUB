27028c3f12f99b96fd97a9b1aef6fe5baa106b28
Otazuki004 <otazuki004@gmail.com>
2025-08-15 05:26:09 +0000

    fix ping

--- Hazel/plugins/ping.py
+++ Hazel/plugins/ping.py
@@ -1,9 +1,18 @@
 from .. import *
-from pyrogram import filters
-import requests
+from pyrogram import filters, raw
 from datetime import datetime
 
 @on_message(filters.command("ping", prefixes=HANDLER) & filters.user('me'))
 async def ping_pong(client, message):
   uptime = datetime.now() - start_time
-  await message.reply_text(f"» Pᴏɴɢ! Rᴇsᴘᴏɴsᴇ ᴛɪᴍᴇ: {await app.ping()} ms\n» Uᴘᴛɪᴍᴇ: {uptime.seconds // 3600}h {(uptime.seconds // 60) % 60}m {uptime.seconds % 60}s")
+
+  ping_start = datetime.now()
+  await client.invoke(
+    raw.functions.ping.Ping(ping_id=client.rnd_id())
+  )
+  ping_ms = (datetime.now() - ping_start).total_seconds() * 1000
+
+  await message.reply(
+    f"» Pᴏɴɢ! Rᴇsᴘᴏɴsᴇ ᴛɪᴍᴇ: {ping_ms:.3f} ms\n"
+    f"» Uᴘᴛɪᴍᴇ: {uptime.seconds // 3600}h {(uptime.seconds // 60) % 60}m {uptime.seconds % 60}s"
+  )
