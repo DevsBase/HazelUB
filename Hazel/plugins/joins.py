d1e8406c0ccdcd66a957f3485ffd972111584d24
Otazuki004 <otazuki004@gmail.com>
2025-08-22 02:45:29 +0000

    i hate git commit

--- Hazel/plugins/joins.py
+++ Hazel/plugins/joins.py
@@ -4,16 +4,16 @@ from pyrogram.errors import InviteRequestSent
 
 @on_message(filters.command(['join','leave'], prefixes=HANDLER) & filters.me)
 async def joins_func(app,m):
-  if (len(m.command) < 2 and m.command[0]=='leave' and m.chat.type not in [enums.ChatType.BOT,enums.ChatType.PRIVATE]):
-    if '-silent' in m.text:
+  if ((len(m.command) < 2 or '-' in m.text) and m.command[0] == 'leave' and m.chat.type not in [enums.ChatType.BOT, enums.ChatType.PRIVATE]):
+    if '-silent' in m.text.lower()
       await m.delete()
     else: await m.reply(f"Left from {m.chat.title}.")    
     await app.leave_chat(m.chat.id)
   elif (len(m.command) < 2):
-    return await m.reply(f'need username/link to {m.command[0]}.')
+    return await m.reply(f'Need username/link to {m.command[0]}.')
   
   link = m.text.split(" ")[1]
-  if m.command[0]=='join':
+  if m.command[0] == 'join':
     try:
       chat = await app.join_chat(link)
       await m.reply(f"Joined, {chat.title}")
