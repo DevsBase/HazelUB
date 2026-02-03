499ae133b78a833306d92df0e3255bef08646f90
Otazuki HS <Otazuki004@gmail.com>
2026-02-03 23:21:38 +0530

    v02.2026

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 API_ID = "10187126"
 API_HASH = "ff197c0d23d7fe54c89b44ed092c1752"
-SESSION = "BQCbcXYANgJd_sGjm7sqzc5vSZatXt4WcZrvjHg-rh6jKYzjlJ-4ck9IYaJIYSJZs1VUxnCYunmMmRndmAUQPV2eEmfSGQ0PS_Czuaoz7Zo40fTzQ1-EHn7Jvs-IKIXRbxIiiLL_TCa1dMvcL3WDlzjkkaBDcRu9PXhuN9LeVltWq_4Rj09KyoGjAPXlUDpsjajB6gCIOoc0rICkmW5xDDek9qS1kNnAkiEH77g88h25YNM_mZ2m5IAyDzEtn3AjpM4p2_BxLq2--_GtEHvkFyf5ZWesuT1CmxhGKZOr6c019gjTxdXnm0qhjOV0xgZOs_nk74TQADiov-24DmULEAKXFa2nigAAAAFji4RfAA"
-BOT_TOKEN = "8361405784:AAGe7luSgRaxinWnIRd-AGwgm6DTJAK2z9c"
-DB_URL = "postgresql+asyncpg://postgres:0507@localhost:5432/postgres"
-OtherSessions = ["BQCbcXYAbJG_z6VWfamE6-bwUbym6P3yOV0FlIOKPUHAjAXxqFHWaKTetBkLn8JaJMwdt9Mt1rrYiNJm0nRH1Rt_onfMSFNs-jSZ2J_S098TAAUHFdO_KKTHeRMUBVwFLMRDC5PaA_oT7w16dd08FTrnRd6tGxNWnSEynk1ZjnsmVjclwu6itpttD1gjbE7yfb7mBIUwtHSRy6hG_3XBvueR5TEdn3zOCu1Mn9lqbfeNchtqTmeawIfRUPJ44Y3wE1IVpI7srRqgrrc6nhCl33BcAc6h7fIlje5nB85Ms52RIjZiOHzW2BE2N2dSQZMZpkKDwPmcg30qdwxihhemJFAN9Te7FwAAAAG3dsePAA"] # Example: ["session1", "session2", "session3"]
+SESSION = ""
+BOT_TOKEN = ""
+DB_URL = "" # PostgreSQL URL
+OtherSessions = [] # Example: ["session1", "session2", ...]
 PREFIX = [".","~","$","^"]
 
 """
