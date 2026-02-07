9b7b1d97525afd17455591486130ebdb19623451
Otazuki HS <Otazuki004@gmail.com>
2026-02-07 16:14:07 +0530

    update: Hazel.Tasks.messageRepeater

--- Hazel/Tasks/messageRepeater.py
+++ Hazel/Tasks/messageRepeater.py
@@ -6,6 +6,7 @@ from Database.client import DBClient
 from Database.Tables.repeatMessage import RepeatMessage
 from pyrogram.errors import FloodWait
 from typing import TYPE_CHECKING, Dict
+import traceback
 
 logger = logging.getLogger("Hazel.Tasks.messageRepeater")
 
@@ -22,19 +23,21 @@ async def createJob(job: RepeatMessage, chats: list, client: Client):
                 await events[job.userId].wait() # type: ignore
             chat_id: int = 0
             for chat_id in chats:
-                await client.copy_message(
-                    chat_id=chat_id,
-                    from_chat_id=job.source_chat_id, # type: ignore
-                    message_id=job.message_id # type: ignore
-                )
-                await asyncio.sleep(2.59)
-        except FloodWait as e:
-            logger.critical(f"FloodWaitError: Sleeping for {e.value}.")
-            await asyncio.sleep(e.value) # type: ignore
+                try:
+                    await client.copy_message(
+                        chat_id=chat_id,
+                        from_chat_id=job.source_chat_id, # type: ignore
+                        message_id=job.message_id # type: ignore
+                    )
+                    await asyncio.sleep(2.59)
+                except FloodWait as e:
+                    logger.critical(f"FloodWaitError: Sleeping for {e.value}.")
+                    await asyncio.sleep(e.value) # type: ignore
+                except Exception as e:
+                    logger.error(f"Could not send repeat message for user: {client.me.id} at chat: {chat_id}. RepeatMessage ID: {job.id}. Error: {e}") # type: ignore
         except Exception as e:
-            logger.error(f"Could not send repeat message for user: {client.me.id} at chat: {chat_id}. RepeatMessage ID: {job.id}. Error: {e}") # type: ignore
-
-
+            logger.error(f"Failed to do repeat task for user: {job.userId}: Full Traceback: {traceback.format_exc()}")
+            
 async def main(Tele: Telegram, db: DBClient):
     jobs = await db.get_repeat_messages()
 
