027bfb9aed62f80d6cd326c34d935e5e7113822b
Otazuki HS <Otazuki004@gmail.com>
2026-02-19 00:17:48 +0530

    feat: add calculator support

---
+++ Mods/calculator.py
@@ -0,0 +1,47 @@
+import ast
+import operator
+import logging
+from Hazel import Tele
+from typing import Any
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+logger = logging.getLogger("Mods.calculator")
+
+allowed_operators = {
+    ast.Add: operator.add,
+    ast.Sub: operator.sub,
+    ast.Mult: operator.mul,
+    ast.Div: operator.truediv
+}
+
+def calculate(expression: str) -> (bool | int | float | Any):
+    node = ast.parse(expression, mode='eval').body
+
+    def evaluate(node):
+        if isinstance(node, ast.Constant):
+            if isinstance(node.value, (int, float)):
+                return node.value
+            else:
+                raise ValueError("Invalid constant")
+
+        elif isinstance(node, ast.BinOp):
+            if type(node.op) not in allowed_operators:
+                raise ValueError("Operator not allowed")
+            left = evaluate(node.left)
+            right = evaluate(node.right)
+            return allowed_operators[type(node.op)](left, right)
+        else:
+            raise ValueError("Invalid expression")
+
+    return evaluate(node)
+
+
+@Tele.on_message(filters.command('/', prefixes='/') & filters.me)
+async def calculateFunc(c: Client, m: Message):
+    exp = str(m.text.split(None, 1)) # type: ignore
+    try:
+        result = calculate(exp)
+        await m.reply(f'{exp} = {result}')
+    except Exception as e:
+        logger.error(f'Failed to calculate: {exp}. Error: {e}')
