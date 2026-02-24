0f2107739dd179c4a3e700ea6bf971d5c3b35cfd
Otazuki HS <Otazuki004@gmail.com>
2026-02-25 01:23:43 +0530

    bug fixes

--- Mods/music.py
+++ Mods/music.py
@@ -135,15 +135,15 @@ def get_music_keyboard(
         [
             [
                 InlineKeyboardButton(pause_resume_text, callback_data=pause_resume_cb),
-                InlineKeyboardButton("⏭ Skip", callback_data=f"mus_skip_{chat_id}"),
+                InlineKeyboardButton("⏭ Skip", callback_data=f"mus_skip_{chat_id}", style=ButtonStyle.PRIMARY),
             ],
             [
-                InlineKeyboardButton("🔁 Loop", callback_data=f"mus_loop_{chat_id}"),
-                InlineKeyboardButton("📜 Queue", callback_data=f"mus_queue_{chat_id}"),
-                InlineKeyboardButton("🛑 Stop", callback_data=f"mus_stop_{chat_id}"),
+                InlineKeyboardButton("🔁 Loop", callback_data=f"mus_loop_{chat_id}", style=ButtonStyle.PRIMARY),
+                InlineKeyboardButton("Queue", callback_data=f"mus_queue_{chat_id}", style=ButtonStyle.PRIMARY),
+                InlineKeyboardButton("🛑 Stop", callback_data=f"mus_stop_{chat_id}", style=ButtonStyle.DANGER),
             ],
             [
-                InlineKeyboardButton("📝 Lyrics", callback_data=f"mus_lyrics_{chat_id}")
+                InlineKeyboardButton("Lyrics", callback_data=f"mus_lyrics_{chat_id}", style=ButtonStyle.PRIMARY),
             ],
             [
                 InlineKeyboardButton(
