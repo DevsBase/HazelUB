bfcb1fa10610e36920921ed7b316ffb2812d23df
Otazuki HS <Otazuki004@gmail.com>
2026-03-20 16:52:16 +0530

    bug fixes

--- Hazel/Platforms/Whatsapp/whatsapp.py
+++ Hazel/Platforms/Whatsapp/whatsapp.py
@@ -19,7 +19,6 @@ class WhatsApp:
             raise ValueError("Arg. client must be neonize.client.NewAClient")
         if client not in self._allClients:
             raise ValueError("Client is must be create with `create_neonize_client` method.")
-        if client.is_logged_in:
-            raise Exception("The client is already logged in.")
+        
         logger.info("Please scan the QR code below. It will expire in 20 sec.")
         asyncio.create_task(client.connect())
