7fb025364b4afcaa44a6d39b1a1ca68f6b27c383
Otazuki HS <Otazuki004@gmail.com>
2026-03-24 12:26:05 +0530

    debug

--- Mods/debug.py
+++ Mods/debug.py
@@ -1,5 +1,6 @@
 
 from neonize.aioze.events import MessageEv
+from neonize.utils.jid import jid_is_lid
 from Hazel import WA
 import logging
 
@@ -8,5 +9,9 @@ async def wsf(client, event: MessageEv):
     logging.info("WA message received")
 
     if event.Message.conversation == ".hi":
-        
-        await client.reply_message(f"Hello {event.Info.MessageSource.Chat}", event)
+        sender = event.Info.MessageSource.Sender
+        if jid_is_lid(sender):
+            sender_alt = event.Info.MessageSource.SenderAlt
+            sender = sender_alt if sender_alt.User else sender
+            phone_number = sender.User
+        await client.reply_message(f"Hello {phone_number}", event)
